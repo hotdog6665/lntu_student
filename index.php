7778459641395e240ff633b98982667ac65f848b
Andrew Ohremchuk <a.ohremchuk97@gmail.com>
2017-04-17 06:57:52 +0000

    index.php edited online with Bitbucket change fields

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
 	 <h2>My name is <?= "Andrew"; ?></h2>
 	 <form name="f1" method="get" action="enter_data.php">
 		<input name="link" type="hidden" value="index.php" />
-		Логин: <br />
+		Login: <br />
 		<input name="login" type="text" size="25" maxlength="30" value="Вася" /> <br />
-		Пароль: <br />
+		Password: <br />
 		<input name="pd" type="password" size="25" maxlength="30" value="" /> <br />
-		<input name="remember" type="checkbox" value="yes" /> Запомнить <br />
+		<input name="remember" type="checkbox" value="yes" /> Remember? <br />
 		<input type="submit" name="enter" value="Вход" />
 	</form>
  </body> 

6df025d4aed8569595838adcaccf1107e7788ecf
andrew ohremchuk <a.ohremchuk97@gmail.com>
2017-04-17 09:45:48 +0300

    add login form in index.php

--- index.php
+++ index.php
@@ -6,5 +6,14 @@
  <body> 
 	 <h1>Hello, today is <?= date('l \t\h\e jS');  ?></h1>
 	 <h2>My name is <?= "Andrew"; ?></h2>
+	 <form name="f1" method="get" action="enter_data.php">
+		<input name="link" type="hidden" value="index.php" />
+		Логин: <br />
+		<input name="login" type="text" size="25" maxlength="30" value="Вася" /> <br />
+		Пароль: <br />
+		<input name="pd" type="password" size="25" maxlength="30" value="" /> <br />
+		<input name="remember" type="checkbox" value="yes" /> Запомнить <br />
+		<input type="submit" name="enter" value="Вход" />
+	</form>
  </body> 
 </html> 
